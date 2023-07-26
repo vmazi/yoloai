80ace5c1832db06059e4c3229be69546cd217b9f
vmazi <vamsikurkal@gmail.com>
2023-07-26 15:47:36 -0400

    even best

--- get_fruit_count.py
+++ get_fruit_count.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 
 def get_item_counts(model_to_use, source_to_use):
-    results = model_to_use.predict(source_to_use, stream=True, verbose=False)  # list of Results objects
+    results = model_to_use.predict(source_to_use, stream=True, verbose=False, device=0)
     results_json = map(lambda res: json.loads(res.tojson()), results)
     return map(lambda inference_list:
                Counter(map(lambda inference: inference['name'],
