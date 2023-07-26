ad992a15db8e9ef3490995b00b10b03d923e8264
vmazi <vamsikurkal@gmail.com>
2023-07-26 13:28:35 -0400

    even best

--- get_fruit_count.py
+++ get_fruit_count.py
@@ -7,10 +7,5 @@ if __name__ == '__main__':
     source = './datasets/coco128/images/train2017/000000000142.jpg'
     results = model.predict(source, stream=True)  # list of Results objects
     results_json = map(lambda res: json.loads(res.tojson()), results)
-    items = chain.from_iterable(results_json)
-    item_names = map(lambda json_item: json_item['name'], items)
-    freq = collections.Counter(item_names)
-
-    for (key, value) in freq.items():
-        print(key, " -> ", value)
-3
+    item_names_by_result = map(lambda json_list: collections.Counter(map(lambda name: name['name'], json_list)), results_json)
+    print(list(item_names_by_result))
