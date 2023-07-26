393b80176cbd4abb214e1f48dae60c4b04bd63dd
vmazi <vamsikurkal@gmail.com>
2023-07-26 13:13:06 -0400

    best

--- get_fruit_count.py
+++ get_fruit_count.py
@@ -1,14 +1,16 @@
 from ultralytics import YOLO
 import json
 import collections
-
+from itertools import chain
 if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
-    source = './datasets/coco128/images/train2017/000000000142.jpg'
-    results = model.predict(source)  # list of Results objects
-    results_json = json.loads(results[0].tojson())
-    item_names = map(lambda json_res: json_res['name'], results_json)
+    source = './datasets/coco128/images/train2017'
+    results = model.predict(source, stream=True)  # list of Results objects
+    results_json = map(lambda res: json.loads(res.tojson()), results)
+    items = chain.from_iterable(results_json)
+    item_names = map(lambda json_item: json_item['name'], items)
     freq = collections.Counter(item_names)
 
     for (key, value) in freq.items():
         print(key, " -> ", value)
+3
